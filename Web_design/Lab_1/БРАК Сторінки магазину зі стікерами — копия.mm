2f7bbd04b3411c1cdde7b31935d5c9800c538f33
Yarik Zaychenko <Yarik@MacBook-Pro-14-2021.local>
2023-10-23 13:16:28 +0300

    Перший коміт, резервна копія

---
+++ Web_design/Lab_1/БРАК Сторінки магазину зі стікерами — копия.mm
@@ -0,0 +1,55 @@
+
+<map version="0.9.0">
+    <node TEXT="Головна сторінка" FOLDED="false" POSITION="right" ID="65259c8fc2596ec4ed22838d" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="17"/>
+        <node TEXT="Паки" FOLDED="false" POSITION="right" ID="a4cd8e7ed64f4becf5a2959a">
+            <edge COLOR="#efa670"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Сторінка з товарами" FOLDED="false" POSITION="right" ID="84f4f8a3326ca7ba3cf966e1">
+                <edge COLOR="#ed9f6a"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Пошук" FOLDED="false" POSITION="right" ID="6b72874fa6c85943e8080dcc">
+            <edge COLOR="#988ee3"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Сторінка з товарами" FOLDED="false" POSITION="right" ID="7372bf05ea2d99db5ad3a226">
+                <edge COLOR="#9289e0"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Персональне замовлення" FOLDED="false" POSITION="left" ID="42acd3a1aacab1c479c8f43e">
+            <edge COLOR="#7aa3e5"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Товар з кастомним паком" FOLDED="false" POSITION="left" ID="a0b3a5ea31e0d449a17cbee3">
+                <edge COLOR="#72a0e5"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Хочу замовити" FOLDED="false" POSITION="right" ID="bf4901f8bdc2d6990a03b78f">
+            <edge COLOR="#ebd95f"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Сторінка з товарами" FOLDED="false" POSITION="right" ID="31e9cc9eb456e1e84cbee13e">
+                <edge COLOR="#edd963"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Дізнатись більше" FOLDED="false" POSITION="right" ID="a9a536aaa4472cfbfe95c020">
+            <edge COLOR="#9ed56b"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Товар з кастомним паком" FOLDED="false" POSITION="right" ID="7c7204dd6b43ed80808308c7">
+                <edge COLOR="#a6d775"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Про нас" FOLDED="false" POSITION="left" ID="7a30275bc8f40271ad3aba1f">
+            <edge COLOR="#e096e9"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Сторінка про компанію" FOLDED="false" POSITION="left" ID="13779c2e5299bc1613b54ec0">
+                <edge COLOR="#dd95e8"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+    </node>
+</map>
